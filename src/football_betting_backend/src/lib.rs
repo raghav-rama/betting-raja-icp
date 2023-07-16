e3d5c0f029562ab45f4ca3bcebb7b8db5819b92f
raghav-rama <hackerer528@gmail.com>
2023-07-16 11:46:37 +0530

    feat: add https outcall routes

--- src/football_betting_backend/src/lib.rs
+++ src/football_betting_backend/src/lib.rs
@@ -1,4 +1,123 @@
+use ic_cdk::api::management_canister::http_request::{
+    http_request, 
+    CanisterHttpRequestArgument, 
+    HttpHeader, 
+    HttpMethod, 
+    // HttpResponse, 
+    // TransformArgs,
+    // TransformContext,
+};
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
+fn greet_me(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+#[ic_cdk::update]
+async fn get_available_games() -> String {
+    let host = String::from("bettingraja-go.onrender.com");
+
+    const PORT: u32 = 443;
+    const ROUTE: &str = "/games";
+
+    let request_headers = vec![
+        HttpHeader {
+            name: String::from("Host"),
+            value: format!("{}:{}", host, PORT, ),
+        },
+        HttpHeader {
+            name: String::from("Content-Type"),
+            value: String::from("application/json"),
+        },
+        HttpHeader {
+            name: String::from("Accept"),
+            value: String::from("application/json"),
+        },
+        HttpHeader {
+            name: String::from("User-Agent"),
+            value: String::from("betting_raja"),
+        },
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: format!("https://{}{}", host, ROUTE),
+        max_response_bytes: None,
+        method: HttpMethod::GET,
+        headers: request_headers,
+        body: None,
+        transform: None,
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            let response_body =
+                String::from_utf8(response.body).expect("response was not valid utf-8");
+            response_body
+        }
+        Err((r, m)) => {
+            let message = format!(
+                "The http_request resulted into error. RejectionCode: {:?}, Message: {:?}",
+                r, m
+            );
+            message
+        }
+    }
+}
+
+#[ic_cdk::update]
+async fn send_bet() -> String {
+    let host = String::from("bettingraja-go.onrender.com");
+
+    const PORT: u32 = 443;
+    const ROUTE: &str = "/games";
+
+    let request_headers = vec![
+        HttpHeader {
+            name: String::from("Host"),
+            value: format!("{}:{}", host, PORT),
+        },
+        HttpHeader {
+            name: String::from("Content-Type"),
+            value: String::from("application/json"),
+        },
+        HttpHeader {
+            name: String::from("User-Agent"),
+            value: String::from("betting_raja"),
+        },
+        HttpHeader {
+            name: String::from("Idempotency-Key"),
+            value: String::from("UUID-1234567890"),
+        }
+    ];
+    let json_string: String = r#"{
+        "name": "Raja",
+        "force_sensitive": "true",
+        "language": "rust"
+    }"#.to_owned();
+    let json_utf8: Vec<u8> = json_string.into_bytes();
+    let request_body: Option<Vec<u8>> = Some(json_utf8);
+
+    let request = CanisterHttpRequestArgument {
+        url: format!("https://{}{}", host, ROUTE, ),
+        max_response_bytes: None,
+        method: HttpMethod::POST,
+        headers: request_headers,
+        body: request_body,
+        transform: None,
+    };
+
+    match http_request(request).await {
+        Ok((response, )) => {
+            let response_body =
+                String::from_utf8(response.body).expect("response was not valid utf-8");
+            response_body
+        }
+        Err((r, m, )) => {
+            let message = format!(
+                "The http_request resulted into error. RejectionCode: {:?}, Message: {:?}",
+                r, m
+            );
+            message
+        }
+    }
 }
---
+++ src/football_betting_frontend/src/index.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { football_betting_backend } from "../../declarations/football_betting_backend";
+
+const App = () => {
+  const [name, setName] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  async function doGreet() {
+    const greeting = await football_betting_backend.greet_me(name);
+    setMessage(greeting);
+  }
+
+  async function doGreet2() {
+    const greeting = await football_betting_backend.get_available_games();
+    setMessage(greeting);
+  }
+
+  return (
+    <>
+      <div style={{ fontSize: "30px" }}>
+        <div style={{ backgroundColor: "pink" }}>
+          <p>Greetings from Betting Raja!</p>
+          <p> Type your name and click the button</p>
+        </div>
+        <div style={{ margin: "30px" }}>
+          <input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button onClick={doGreet}>Greet</button>
+          <button onClick={doGreet2}>Greet2</button>
+          <div>
+            <p>Message from backend:</p>
+            <p>{message}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+render(<App />, document.getElementById("app"));
